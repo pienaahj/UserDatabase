34a110e9a1bb468cfead3bfa9107d17c69fa8116
Hendrik Pienaar <hendrikpienaar@Henriettes-iMac.local>
2022-06-01 11:34:13 +0200

    Separated db code out to databases package and added gomock generator code for interfaces

--- UserDatabase_test.go
+++ UserDatabase_test.go
@@ -1,11 +1,15 @@
 package userdatabase_test
 
 import (
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/pienaahj/UserDatabase/mocks/mock_UserDatabase"
+	mock_UserDatabase "github.com/pienaahj/UserDatabase/mocks"
+	"github.com/pienaahj/UserDatabase/models"
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // set up the TestSuite
@@ -20,3 +24,27 @@ func TestUserDatabaseSuite(t *testing.T) {
 // setup function for gomockcontroller
 var ctrl *gomock.Controller
 var u *mock_UserDatabase.MockUserDatabase
+
+// setup the Test function
+func (s *UserDatabaseSuite) SetupTest() {
+	ctrl = gomock.NewController(s.T())
+	u = mock_UserDatabase.NewMockUserDatabase(ctrl)
+
+}
+
+// clean up the TestSuite
+func (u *UserDatabaseSuite) TearDown() {
+	ctrl.Finish()
+}
+
+func (s *UserDatabaseSuite) TestUserDatabase() {
+	ctx := context.Background()
+	username := "mocked-db-user"
+	usr := &models.User{Username: username}
+	filter := bson.M{"error": true}
+	filterNoError := bson.M{"error": false}
+	u.EXPECT().Create(ctx, usr).Return(nil)
+	u.EXPECT().FindOne(ctx, filter).Return(nil, errors.New("mocked-user-db-error"))
+	u.EXPECT().FindOne(ctx, filterNoError).Return(usr, nil)
+	u.EXPECT().DeleteByUsername(ctx, username).Return(nil)
+}
---
+++ databases/databases.go
@@ -0,0 +1,174 @@
+package databases
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+const (
+	DatabaseName   string = "testdb"
+	CollectionName string = "movies"
+	// connectionStringAdmin string = "mongodb://admin:myadminpassword@192.168.0.148:27017" works for local lan clients
+	connectionStringAdmin string = "mongodb://admin:myadminpassword@localhost:27017" // works for iMac clients
+	connectionStringUser  string = "mongodb://user2:user2password@192.168.0.148:27017/user2?authSource=testdb"
+)
+
+var (
+	DBError   error
+	adminUser options.Credential = options.Credential{
+		Username:   "admin",
+		Password:   "myadminpassword",
+		AuthSource: "admin",
+	}
+	user2User options.Credential = options.Credential{
+		Username:   "user2",
+		Password:   "user2password",
+		AuthSource: "testdb",
+	}
+	adminURI string = connectionStringAdmin
+	// user2URI string = "192.168.0.148:27017/user2"
+)
+
+//go:generate mockgen --destination=./mocks/databaseHelper.go github.com/pienaahj/databases DatabaseHelper
+type DatabaseHelper interface {
+	Collection(name string) CollectionHelper
+	Client() ClientHelper
+}
+
+//go:generate mockgen --destination=./mocks/collectionHelper.go github.com/pienaahj/databases CollectionHelper
+type CollectionHelper interface {
+	FindOne(context.Context, interface{}) SingleResultHelper
+	FindMany(ctx context.Context, filter interface{}) (MongoResults, error)
+	InsertOne(context.Context, interface{}) (interface{}, error)
+	DeleteOne(ctx context.Context, filter interface{}) (int64, error)
+}
+
+//go:generate mockgen --destination=./mocks/singleResultHelper.go github.com/pienaahj/databases SingleResultHelper
+type SingleResultHelper interface {
+	Decode(v interface{}) error
+}
+
+//go:generate mockgen --destination=./mocks/clientHelper.go github.com/pienaahj/databases ClientHelper
+type ClientHelper interface {
+	Database(string) DatabaseHelper
+	Connect(context.Context) error
+	StartSession() (mongo.Session, error)
+	CheckConnection(context.Context) error
+}
+
+type mongoClient struct {
+	cl *mongo.Client
+}
+type mongoDatabase struct {
+	db *mongo.Database
+}
+type mongoCollection struct {
+	coll *mongo.Collection
+}
+
+type mongoSingleResult struct {
+	sr *mongo.SingleResult
+}
+
+type MongoResults struct {
+	RS []bson.M
+}
+
+type mongoSession struct {
+	mongo.Session
+}
+
+func NewClient() (ClientHelper, error) {
+	c, err := mongo.NewClient(options.Client().SetAuth(
+		options.Credential{
+			Username:   adminUser.Username,
+			Password:   adminUser.Password,
+			AuthSource: adminUser.AuthSource,
+		}).ApplyURI(adminURI))
+
+	return &mongoClient{cl: c}, err
+
+}
+
+// func NewDatabase(cnf *config.Config, client ClientHelper) DatabaseHelper {
+// 	return client.Database(cnf.DatabaseName)
+// }
+
+func (mc *mongoClient) CheckConnection(ctx context.Context) error {
+	err := mc.cl.Ping(ctx, readpref.Primary())
+	if err != nil {
+		fmt.Println("Could not connect to mongo client")
+		return fmt.Errorf("could not connect to mongo client, %v", err)
+	}
+	return nil
+}
+
+func (mc *mongoClient) Database(dbName string) DatabaseHelper {
+	db := mc.cl.Database(dbName)
+	return &mongoDatabase{db: db}
+}
+
+func (mc *mongoClient) StartSession() (mongo.Session, error) {
+	session, err := mc.cl.StartSession()
+	return &mongoSession{session}, err
+}
+
+func (mc *mongoClient) Connect(ctx context.Context) error {
+	return mc.cl.Connect(ctx)
+}
+
+func (md *mongoDatabase) Collection(colName string) CollectionHelper {
+	collection := md.db.Collection(colName)
+	return &mongoCollection{coll: collection}
+}
+
+func (md *mongoDatabase) Client() ClientHelper {
+	client := md.db.Client()
+	return &mongoClient{cl: client}
+}
+
+func (mc *mongoCollection) FindOne(ctx context.Context, filter interface{}) SingleResultHelper {
+	singleResult := mc.coll.FindOne(ctx, filter)
+	return &mongoSingleResult{sr: singleResult}
+}
+
+func (mc *mongoCollection) FindMany(ctx context.Context, filter interface{}) (MongoResults, error) {
+	var results []bson.M
+	curr, err := mc.coll.Find(ctx, filter)
+	if err != nil {
+		return MongoResults{}, fmt.Errorf("an error occured while finding %v", filter)
+	}
+	defer curr.Close(ctx)
+	for curr.Next(context.TODO()) {
+		var ir bson.M
+		err = curr.Decode((&ir))
+		if err != nil {
+			return MongoResults{}, fmt.Errorf("an error occured while decoding items %v", err)
+		}
+		results = append(results, ir)
+	}
+	if err := curr.Err(); err != nil {
+		return MongoResults{}, fmt.Errorf("an error occured on cursor %v", err)
+	}
+
+	return MongoResults{RS: results}, nil
+}
+
+func (mc *mongoCollection) InsertOne(ctx context.Context, document interface{}) (interface{}, error) {
+	id, err := mc.coll.InsertOne(ctx, document)
+	return id.InsertedID, err
+}
+
+func (mc *mongoCollection) DeleteOne(ctx context.Context, filter interface{}) (int64, error) {
+	count, err := mc.coll.DeleteOne(ctx, filter)
+	return count.DeletedCount, err
+}
+
+func (sr *mongoSingleResult) Decode(v interface{}) error {
+	return sr.sr.Decode(v)
+}
